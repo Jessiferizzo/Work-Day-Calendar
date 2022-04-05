4eaa664dc1fc25b750b732ecab8bdf2e6fbf9374
Zoey <jsisavath@gmail.com>
2022-04-05 12:44:56 -0500

    fixed present time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ currentDateEl.textContent= moment().format('dddd')+","+"   "+moment().format("MM
 //Change textarea background color based on time
 var checkTime = function () {
     //Get Current time
-    var currentTime = moment().format("HH:mm");
+    var currentTime = moment().hours()
     console.log(currentTime);
 
     //loop through description classes
@@ -36,66 +36,20 @@ var checkTime = function () {
         var newId = document.getElementById(timeBlockEvents[i].id)
         console.log(newId);
 
-        //remove any old classes from element
-        $(timeBlockEvents[i].id).removeClass(".present .past .future");
-
-        // apply new class if task is present/past/future
-        if (currentTime.isAfter(elementId) && elementId.isBefore(currentTime)) {
-            $(newId).addClass("present");
-            console.log("it's the present")
-        } else if (elementId < currentTime) {
-            $(newId).addClass("past");
-            console.log("its the past");
-        } else {
-            $(newId).addClass("future");
-            console.log("its the future!)");
-        }
+       // apply new class if task is present/past/future
+       if (elementId==currentTime) {
+        $(newId).addClass("present");
+        console.log("it's the present")
+    } else if (elementId < currentTime) {
+        $(newId).addClass("past");
+        console.log("its the past");
+    } else {
+        $(newId).addClass("future");
+        console.log("its the future!)");
     }
+}
 }
 console.log("time was checked!");
 // checkTime every 5 minutes
 setInterval(checkTime(), (1000 * 60) * 5);
 
-
-//WHEN I click into a time block
-//THEN I can enter an event HTML DID IT
-        
-//WHEN I click the save button for that time block
-   //save button active 
-     // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery, how does this work//
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        // save event to local storage
-        localStorage.setItem(time, text);
-    })
-    console.log("you saved it!");
-
-/*function textSubmit(event) {
-    event.preventDefault();
-    var textEvent = document.querySelector('.description');
-    var textEvent= textEvent.textContent
-    console.log(textEvent);
-}
-console.log(textSubmit);
-
-  var submitButton= document.querySelector('.saveBtn')
-  submitButton.addEventListener('submit', textSubmit);
-  console.log("you clicked save!");
-
-//THEN the text for that event is saved in local storage
-   //save textcontent to local storage
-/*var saveTasks = function() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};*/
-
-// audit task due dates every 5 minutes
-/*setInterval(checkTime(), (1000 * 60) * 5);*/
-
-//WHEN I refresh the page
-//THEN the saved events persist
-  //tasks stay present on page after refresh without clearing*/
-/*var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));*/
